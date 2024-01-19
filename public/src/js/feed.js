3e3abb2d1858aed8618d7fc2521421dc7d69828c
Ferhat Adıbelli <ferhatadibelli9@gmail.com>
2024-01-20 01:17:46 +0300

    cache strategies applied

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -3,23 +3,33 @@ var createPostArea = document.querySelector('#create-post');
 var closeCreatePostModalButton = document.querySelector(
   '#close-create-post-modal-btn'
 );
+var sharedMomentsArea = document.querySelector('#shared-moments');
 
 function openCreatePostModal() {
   createPostArea.style.display = 'block';
-  // This is because we want to show the prompt after this click occurs
-  if (deferredEvent) {
-    deferredEvent.prompt();
+  if (deferredPrompt) {
+    deferredPrompt?.prompt();
 
-    deferredEvent.userChoice.then((choiceResult) => {
+    deferredPrompt?.userChoice?.then(function (choiceResult) {
+      console.log(choiceResult.outcome);
+
       if (choiceResult.outcome === 'dismissed') {
-        console.log('User dismissed the prompt');
+        console.log('User cancelled installation');
       } else {
-        console.log('User added the prompt');
+        console.log('User added to home screen');
       }
     });
 
-    deferredEvent = null;
+    deferredPrompt = null;
   }
+
+  // if ('serviceWorker' in navigator) {
+  //   navigator.serviceWorker.getRegistrations().then((registrations) => {
+  //     for (let i = 0; i < registrations.length; i++) {
+  //       registrations[i].unregister();
+  //     }
+  //   });
+  // }
 }
 
 function closeCreatePostModal() {
@@ -29,3 +39,85 @@ function closeCreatePostModal() {
 shareImageButton.addEventListener('click', openCreatePostModal);
 
 closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
+
+//This is unused now to cache some data occationally
+function onSaveButtonClicked(event) {
+  console.log('clicked');
+  if ('caches' in window) {
+    caches.open('user-react').then((cache) => {
+      cache.addAll(['https://httpbin.org/get', '/src/images/sf-boat.jpg']);
+    });
+  } else {
+    console.log('Caches dont exist');
+  }
+}
+
+function clearCard() {
+  if (sharedMomentsArea.hasChildNodes()) {
+    sharedMomentsArea.removeChild(sharedMomentsArea.lastChild);
+  }
+}
+
+function createCard() {
+  var cardWrapper = document.createElement('div');
+  cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
+  var cardTitle = document.createElement('div');
+  cardTitle.className = 'mdl-card__title';
+  cardTitle.style.backgroundImage = 'url("/src/images/sf-boat.jpg")';
+  cardTitle.style.backgroundSize = 'cover';
+  cardTitle.style.height = '180px';
+  cardWrapper.appendChild(cardTitle);
+  var cardTitleTextElement = document.createElement('h2');
+  cardTitleTextElement.style.color = 'white';
+  cardTitleTextElement.className = 'mdl-card__title-text';
+  cardTitleTextElement.textContent = 'San Francisco Trip';
+  cardTitle.appendChild(cardTitleTextElement);
+  var cardSupportingText = document.createElement('div');
+  cardSupportingText.className = 'mdl-card__supporting-text';
+  cardSupportingText.textContent = 'In San Francisco';
+  cardSupportingText.style.textAlign = 'center';
+  // var cardSaveButton = document.createElement('button');
+  // cardSaveButton.textContent = 'Save!';
+  // cardSaveButton.addEventListener('click', onSaveButtonClicked);
+  // cardSupportingText.appendChild(cardSaveButton);
+  cardWrapper.appendChild(cardSupportingText);
+  componentHandler.upgradeElement(cardWrapper);
+  sharedMomentsArea.appendChild(cardWrapper);
+}
+let dataFromWeb = false;
+const api = 'https://httpbin.org/post';
+
+fetch(api, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+  body: JSON.stringify({ message: 'Something that is not important.' }),
+})
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (data) {
+    console.log('From web :', data);
+    dataFromWeb = true;
+    clearCard();
+    createCard();
+  });
+
+if ('caches' in window) {
+  caches
+    .match(api)
+    .then((response) => {
+      if (response) {
+        return response.json();
+      }
+    })
+    .then((data) => {
+      console.log('From caches', data);
+      if (!dataFromWeb) {
+        clearCard();
+        createCard();
+      }
+    });
+}
--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,160 @@
+const CACHE_STATIC_NAME = 'static-v2';
+const CACHE_DYNAMIC_NAME = 'dynamic-v2';
+const STATIC_ASSETS = [
+  '/',
+  '/index.html',
+  '/offline.html',
+  '/src/js/app.js',
+  '/src/js/feed.js',
+  '/src/js/promise.js',
+  '/src/js/fetch.js',
+  '/src/js/material.min.js',
+  '/src/css/app.css',
+  '/src/css/feed.css',
+  '/src/images/main-image.jpg',
+  'https://fonts.googleapis.com/css?family=Roboto:400,700',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+];
+
+function trimCache(cacheName, maxSize) {
+  caches.open(cacheName).then((cache) => {
+    return cache.keys().then((keys) => {
+      if (keys.length > maxSize) {
+        cache.delete(keys[0]).then(() => {
+          trimCache(cacheName, maxSize);
+        });
+      }
+    });
+  });
+}
+
 self.addEventListener('install', (event) => {
-  console.log('[serviceWorker] is installed ', event);
+  console.log('[serviceWorker] is installed ');
+  event.waitUntil(
+    caches.open(CACHE_STATIC_NAME).then((cache) => {
+      console.log('[serviceWorker] precaching app shell');
+      cache.addAll(STATIC_ASSETS);
+    })
+  );
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('[serviceWorker] is activated ', event);
+  //?This event is triggered when user close all active tabs and open up new one
+  console.log('[serviceWorker] is activated');
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+            console.log('Deleted key :', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
   return self.clients.claim();
 });
 
+//? Cache-with-network-fallback strategy-1
+// self.addEventListener('fetch', (event) => {
+//   // event.respondWith(null);
+//   event.respondWith(
+//     caches.match(event.request).then((result) => {
+//       if (result) {
+//         return result;
+//       } else {
+//         return fetch(event.request)
+//           .then((response) => {
+//             return caches.open(CACHE_DYNAMIC_NAME).then((cache) => {
+//               cache.put(event.request.url, response.clone());
+//               return response;
+//             });
+//           })
+//           .catch((err) => {
+//             return caches.match('/offline.html');
+//           });
+//       }
+//     })
+//   );
+// });
+
+//? Cache-only strategy-2
+// self.addEventListener('fetch', (event) => {
+//   event.respondWith(caches.match(event.request));
+// });
+
+//? Network-only strategy-3
+// self.addEventListener('fetch', (event) => {
+//   event.respondWith(fetch(event.request));
+// });
+
+//? Network-with-caches-fallback strategy-4
+// self.addEventListener('fetch', (event) => {
+//   event.respondWith(
+//     fetch(event.request)
+//       .then((response) => {
+//         return caches.open(CACHE_DYNAMIC_NAME).then((cache) => {
+//           cache.put(event.request.url, response.clone());
+//           return response;
+//         });
+//       })
+//       .catch((err) => {
+//         caches.match(event.request);
+//       })
+//   );
+// });
+
+function isInArray(string, array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === string) {
+      return true;
+    }
+  }
+  return false;
+}
+
+//? Mix of Cache-then-network strategy-5 and Cache-with-network-fallback strategy-1
 self.addEventListener('fetch', (event) => {
-  console.log('[serviceWorker] fetch is triggered ', event);
-  // event.respondWith(null);
-  event.respondWith(fetch(event.request));
+  const api = 'https://httpbin.org/get';
+  if (event.request.url.indexOf(api) !== -1) {
+    //? Cache-then-network strategy-5 (Check cache inside frontend)
+    event.respondWith(
+      fetch(event.request).then((response) => {
+        return caches.open(CACHE_DYNAMIC_NAME).then((cache) => {
+          trimCache(CACHE_DYNAMIC_NAME, 3);
+          cache.put(event.request.url, response.clone());
+          return response;
+        });
+      })
+    );
+  } else if (isInArray(event.request.url, STATIC_ASSETS)) {
+    event.respondWith(caches.match(event.request));
+  } else {
+    //? Cache-with-network-fallback strategy-1
+    event.respondWith(
+      caches.match(event.request).then((result) => {
+        if (result) {
+          return result;
+        } else {
+          return fetch(event.request)
+            .then((response) => {
+              return caches.open(CACHE_DYNAMIC_NAME).then((cache) => {
+                trimCache(CACHE_DYNAMIC_NAME, 4);
+                cache.put(event.request.url, response.clone());
+                return response;
+              });
+            })
+            .catch((err) => {
+              if (event.request.headers.get('accept').includes('text/html')) {
+                return caches.open(CACHE_STATIC_NAME).then((cache) => {
+                  return cache.match('/offline.html');
+                });
+              }
+            });
+        }
+      })
+    );
+  }
 });
